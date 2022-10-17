439d447ab522c8b4d639eb8be7aee1798ca66ec4
LEMASSON Louis <galou2608@gmail.com>
2022-10-17 16:53:23 +0200

    Release 1

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-console.log("Hello World !");
-
-export function helloWorld() {
-    console.log("Hello World !");
-}
-
-export { isUndefined } from "./src/isUndefined";
+export { isUndefined } from "./src/isUndefined.js";
