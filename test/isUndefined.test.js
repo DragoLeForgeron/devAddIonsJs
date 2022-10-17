a97693c715c2325bedc2ee002784b140e0f3f63e
LEMASSON Louis <galou2608@gmail.com>
2022-10-17 13:42:43 +0200

    Add more tests to isUndefined.test.js

--- test/isUndefined.test.js
+++ test/isUndefined.test.js
@@ -2,12 +2,42 @@ import { expect } from "chai"
 import { describe, it } from "mocha";
 import { isUndefined } from "../src/isUndefined.js";
 
-describe("tests isUnefined", () => {
+describe("tests isUndefined", () => {
 
     it("Should return true", () => {
         const value = undefined;
         const result = isUndefined(value);
-        expect(result).to.equal(true);
+        expect(result).to.be.true;
     });
 
-})
+    it("Should return true", () => {
+        let value;
+        const result = isUndefined(value);
+        expect(result).to.be.true;
+    });
+
+    it("Should return false", () => {
+        const value = "";
+        const result = isUndefined(value);
+        expect(result).to.be.false;
+    });
+
+    it("Should return false", () => {
+        const value = null;
+        const result = isUndefined(value);
+        expect(result).to.be.false;
+    });
+
+    it("Should return false", () => {
+        const value = 0;
+        const result = isUndefined(value);
+        expect(result).to.be.false;
+    });
+
+    it("Should return false", () => {
+        const value = true;
+        const result = isUndefined(value);
+        expect(result).to.be.false;
+    });
+
+});
