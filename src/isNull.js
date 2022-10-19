3e71cfccef54c200d8617403af16f8039cf08f57
LEMASSON Louis <galou2608@gmail.com>
2022-10-19 15:43:55 +0200

    Create "isNull"

---
+++ src/isNull.js
@@ -0,0 +1,8 @@
+/**
+ * Return if a varaible is null.
+ * @param {any} variable - Any variable to know if it null.
+ * @returns {Boolean} Boolean
+ */
+export function isNull(variable) {
+    return variable === null;
+}
