325e128ca6418d02112fd5a9f9ab44cef2a2f769
LEMASSON Louis <galou2608@gmail.com>
2022-10-19 17:41:11 +0200

    Update areNull.test.js according to the new format function

--- test/areNull.test.js
+++ test/areNull.test.js
@@ -1,6 +1,7 @@
 import { expect } from "chai"
 import { describe, it } from "mocha";
 import { areNull } from "../src/areNull.js";
+import { formatTestValue } from "./setup.js";
 
 describe("Tests areNull()", () => {
 
@@ -15,7 +16,7 @@ describe("Tests areNull()", () => {
 
     for(const value2 of badValues) {
         const value1 = null;
-        it(`Should \`${value1}\` and \`${value2}\` return false`, () => {
+        it(`Should \`${value1}\` and \`${formatTestValue(value2)}\` return false`, () => {
             const result = areNull(value1, value2);
             expect(result).to.be.false;
         });
