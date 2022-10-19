00bbf22f70cc4fc91a2cf61b0a1988be5f48d885
LEMASSON Louis <galou2608@gmail.com>
2022-10-19 15:00:57 +0200

    Add more tests

--- test/areUndefined.test.js
+++ test/areUndefined.test.js
@@ -11,4 +11,28 @@ describe("Tests areUndefined()", () => {
         expect(result).to.be.true;
     });
 
+    it("Should `undefined` and `<notSet>` return true", () => {
+        const value1 = undefined;
+        let value2;
+        const result = areUndefined(value1, value2);
+        expect(result).to.be.true;
+    });
+
+    it("Should `<notSet>` and `<notSet>` return true", () => {
+        let value1;
+        let value2;
+        const result = areUndefined(value1, value2);
+        expect(result).to.be.true;
+    });
+
+    const badValues = ["", null, 0, true, [], {}];
+
+    for(const value2 of badValues) {
+        const value1 = undefined;
+        it(`Should \`${value1}\` and \`${value2}\` return false`, () => {
+            const result = areUndefined(value1, value2);
+            expect(result).to.be.false;
+        });
+    }
+
 });
