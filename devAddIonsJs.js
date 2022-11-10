bb5814d1ad95635a28e21f3c091b538389570733
LEMASSON Louis <galou2608@gmail.com>
2022-11-10 14:20:33 +0100

    test export

--- devAddIonsJs.js
+++ devAddIonsJs.js
@@ -1,4 +1,11 @@
-export { isUndefined } from "./src/isUndefined.js";
-export { areUndefined } from "./src/areUndefined.js";
-export { isNull } from "./src/isNull.js";
-export { areNull } from "./src/areNull.js";
+import { isUndefined } from "./src/isUndefined.js";
+import { areUndefined } from "./src/areUndefined.js";
+import { isNull } from "./src/isNull.js";
+import { areNull } from "./src/areNull.js";
+
+export {
+    isUndefined,
+    areUndefined,
+    isNull,
+    areNull
+}
